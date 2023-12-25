1b498cbe888f6ddf6aa263448a68ef8ca8ee2c04
mattbasile <matthewbasile2@gmail.com>
2023-12-24 20:34:52 -0500

    wip

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
   let currentCard = 1;
   const totalCards = 3;
   const nextButton = document.getElementById('nextCard');
+  const intro = document.getElementById('intro');
+  const enter = document.getElementById('enter');
 
   nextButton.addEventListener('click', () => {
     // Hide the current card
@@ -12,5 +14,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Show the new card
     document.getElementById('card' + currentCard).style.display = 'block';
+  });
+
+  enter.addEventListener('click', () => {
+    intro.style.display = 'none';
   });
 });
